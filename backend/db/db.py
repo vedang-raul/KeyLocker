59ce2fb399fc35adc68e7f472c98ef38afa633cd
Vedang <vedangvishalrauL@gmail.com>
2026-01-18 16:25:48 +0530

    created script for testing db

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,15 +1,19 @@
 import fastapi as FastAPI
 from sqlalchemy.orm import sessionmaker,declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-engine=create_async_engine(os.getenv("DIRECT_URL"))
+engine = create_async_engine(os.getenv("DIRECT_URL"))
 
 
-SessionLocal=sessionmaker(bind=engine)
+SessionLocal = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
+)
 
 Base=declarative_base()
 
